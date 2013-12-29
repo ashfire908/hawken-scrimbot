3eb314086a4df7329932212a5f72129523f5f0b8
Andrew Hampe <andrew.hampe@gmail.com>
2013-12-28 19:32:39 -0600

    Test: Added guid/callsign lookup commands
    Also tweaked a message.

--- scrimbot/plugins/test.py
+++ scrimbot/plugins/test.py
@@ -13,13 +13,16 @@ class TestPlugin(BasePlugin):
         self.register_command(CommandType.PM, "testexception", self.test_exception, flags=["hidden", "safe"])
         self.register_command(CommandType.ALL, "hammertime", self.hammertime, flags=["hidden", "safe"])
         self.register_command(CommandType.ALL, "whoami", self.whoami)
+        self.register_command(CommandType.ALL, "callsign", self.callsign, flags=["hidden"])
+        self.register_command(CommandType.ALL, "guid", self.guid, flags=["hidden"])
         self.register_command(CommandType.PM, "tell", self.tell, flags=["permsreq"], metadata={"permsreq": ["admin"]})
 
     def disable(self):
         # Unregister commands
         self.unregister_command(CommandType.PM, "testexception")
         self.unregister_command(CommandType.ALL, "hammertime")
-        self.unregister_command(CommandType.ALL, "whoami")
+        self.unregister_command(CommandType.ALL, "callsign")
+        self.unregister_command(CommandType.ALL, "guid")
         self.unregister_command(CommandType.PM, "tell")
 
     def connected(self):
@@ -46,10 +49,42 @@ class TestPlugin(BasePlugin):
 
         self.xmpp.send_message(cmdtype, target, message)
 
+    def callsign(self, cmdtype, cmdname, args, target, user, room):
+        # Check args
+        if len(args) < 1:
+            self.xmpp.send_message(cmdtype, target, "Missing target user guid.")
+        else:
+            # Get the callsign
+            callsign = self.cache.get_callsign(args[0])
+
+            # Check if we got a callsign back
+            if callsign is None:
+                message = "Error: No callsign found for given GUID."
+            else:
+                message = "User GUID resolves to '{0}'.".format(callsign)
+
+            self.xmpp.send_message(cmdtype, target, message)
+
+    def guid(self, cmdtype, cmdname, args, target, user, room):
+        # Check args
+        if len(args) < 1:
+            self.xmpp.send_message(cmdtype, target, "Missing target user callsign.")
+        else:
+            # Get the guid
+            guid = self.cache.get_guid(args[0])
+
+            # Check if we got a guid back
+            if guid is None:
+                message = "Error: No user found for given callsign."
+            else:
+                message = "User callsign resolves to '{0}'.".format(guid)
+
+            self.xmpp.send_message(cmdtype, target, message)
+
     def tell(self, cmdtype, cmdname, args, target, user, room):
         # Check the arguments
         if len(args) < 2:
-            self.xmpp.send_message(cmdtype, target, "Missing target user and/or message")
+            self.xmpp.send_message(cmdtype, target, "Missing target user and/or message.")
         else:
             callsign = args[0]
             message = " ".join(args[1:])
