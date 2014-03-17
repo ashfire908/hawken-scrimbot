681e967adac52c0590af30a7db473a3dd0856ade
Andrew Hampe <andrew.hampe@gmail.com>
2014-03-17 16:46:26 -0500

    Quality: Added qualitysearch command

--- scrimbot/plugins/quality.py
+++ scrimbot/plugins/quality.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 
 import logging
+from scrimbot.api import region_names, gametype_names, get_region, get_gametype
 from scrimbot.command import CommandType
 from scrimbot.plugins.base import BasePlugin
 from scrimbot.util import calc_fitness
@@ -19,10 +20,13 @@ class QualityPlugin(BasePlugin):
         self.register_config("plugins.quality.min_users", 2)
         self.register_config("plugins.quality.log_usage", False)
         self.register_config("plugins.quality.health_offset", 100)
+        self.register_config("plugins.quality.target_results", 3)
+        self.register_config("plugins.quality.max_results", 6)
 
         # Register commands
         self.register_command(CommandType.ALL, "quality", self.quality, alias=["qa"])
         self.register_command(CommandType.ALL, "qualityexplain", self.quality_explain, alias=["qae"])
+        self.register_command(CommandType.ALL, "qualitysearch", self.quality_search, alias=["qs"])
 
     def disable(self):
         pass
@@ -177,6 +181,107 @@ class QualityPlugin(BasePlugin):
 
                     # Log it
                     self.record_usage(cmdname, result[0], server_info, data=details)
+
+    def quality_search(self, cmdtype, cmdname, args, target, user, party):
+        blacklist = ["HawkenCoOp"]
+
+        # Check the arguments
+        if len(args) < 1:
+            self._xmpp.send_message(cmdtype, target, "Error: Missing target region.")
+            return
+
+        region = get_region(args[0])
+        if not region:
+            self._xmpp.send_message(cmdtype, target, "Error: Invalid target region.")
+            return
+        if len(args) > 1:
+            gametype = get_gametype(args[1])
+            if not gametype:
+                self._xmpp.send_message(cmdtype, target, "Error: Invalid gametype.")
+                return
+
+            if gametype in blacklist:
+                self._xmpp.send_message(cmdtype, target, "Error: Searching for {0} is disabled.".format(gametype_names[gametype]))
+                return
+        else:
+            gametype = None
+
+        # Get the server list
+        server_list = self._api.get_server()
+
+        # Load the player data
+        player = self._api.get_user_stats(user)
+
+        if player is None:
+            self._xmpp.send_message(cmdtype, target, "Error: Failed to load player stats.")
+        else:
+            # Filter the servers
+            def server_filter(server):
+                if len(server["Users"]) == 0:
+                    # Empty server
+                    return False
+
+                if server["Region"].lower() != region.lower():
+                    # Region does not match
+                    return False
+
+                if server["GameType"] in blacklist:
+                    # Gametype is blacklisted
+                    return False
+
+                if gametype is not None and server["GameType"] != gametype:
+                    # Gametype does not match
+                    return False
+
+                return True
+
+            servers = [server for server in server_list if server_filter(server)]
+
+            # Get the header identifier
+            if gametype is None:
+                identifier = region_names[region]
+            else:
+                identifier = "{0} {1}".format(region_names[region], gametype_names[gametype])
+
+            # Check if we have any servers left
+            if len(servers) == 0:
+                lines = ["No {0} servers found.".format(identifier)]
+            else:
+                # Calculate the server fitness
+                server_fitness = {}
+
+                def get_fitness(server):
+                    fitness = calc_fitness(self._cache["globals"], player, server)
+                    server_fitness[server["Guid"]] = fitness[3]
+
+                    return abs(fitness[0])
+                results = sorted(servers, key=get_fitness)[:self._config.plugins.quality.max_results]
+
+                # Format the output
+                lines = []
+                x = 0
+                for server in results:
+                    if x >= self._config.plugins.quality.target_results:
+                        break
+
+                    fitness = server_fitness[server["Guid"]]
+
+                    if self._config.plugins.quality.health_offset:
+                        # Offset the health
+                        health = -fitness["health"] + self._config.plugins.quality.health_offset
+                    else:
+                        health = fitness["health"]
+
+                    lines.append("{0[ServerName]}: Quality {1} ({2}) - {3} - Players {4}/{0[MaxUsers]}".format(server, fitness["rating"], health, gametype_names[server["GameType"]], len(server["Users"])))
+
+                    if len(server["Users"]) < server["MaxUsers"]:
+                        x += 1
+
+                # Add the header
+                lines.insert(0, "Found {0} {1} servers by fitness:".format(len(lines), identifier))
+
+            # Return the results
+            self._xmpp.send_message(cmdtype, target, "\n".join(lines))
 
 
 plugin = QualityPlugin
